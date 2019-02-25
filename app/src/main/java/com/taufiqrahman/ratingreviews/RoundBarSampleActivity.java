5b240bdfd2289d5214cb11c99c2ecd9475b12ac5
Inconnu08 <taufiqrx8@gmail.com>
2019-02-25 15:39:05 +0600

    update RoundBarSampleActivity

--- app/src/main/java/com/taufiqrahman/ratingreviews/RoundBarSampleActivity.java
+++ app/src/main/java/com/taufiqrahman/ratingreviews/RoundBarSampleActivity.java
@@ -17,13 +17,6 @@ public class RoundBarSampleActivity extends AppCompatActivity {
 
         RatingReviews ratingReviews = (RatingReviews) findViewById(R.id.rating_reviews);
 
-        int colors[] = new int[]{
-                Color.parseColor("#0e9d58"),
-                Color.parseColor("#bfd047"),
-                Color.parseColor("#ffc105"),
-                Color.parseColor("#ef7e14"),
-                Color.parseColor("#d36259")};
-
         int raters[] = new int[]{
                 new Random().nextInt(100),
                 new Random().nextInt(100),
@@ -32,6 +25,6 @@ public class RoundBarSampleActivity extends AppCompatActivity {
                 new Random().nextInt(100)
         };
 
-        ratingReviews.createRatingBars(100, BarLabels.STYPE1, colors, raters);
+        ratingReviews.createRatingBars(100, BarLabels.STYPE3, Color.parseColor("#0f9d58"), raters);
     }
 }
